ecd6e22b1586f3e4f8e1fcaff5261dcca8f89b80
emjshrx <emjshrx@gmail.com>
2023-08-27 13:06:52 +0530

    build: modify build instructions

--- postinstall.js
+++ postinstall.js
@@ -4,9 +4,9 @@ var os = require('os');
 const baseCommand = 'rn-nodeify --install buffer,stream,assert,events,crypto,vm,process --hack';
 
 function postInstallMac() {
-  //   exec(`${baseCommand} && cd ios && pod install && cd ..`);
-  // M1 workaround
-  exec(`${baseCommand} && cd ios && arch -x86_64 pod install && cd ..`);
+  exec(`${baseCommand} && cd ios && pod install && cd ..`);
+  // // M1 workaround
+  // exec(`${baseCommand} && cd ios && arch -x86_64 pod install && cd ..`);
 }
 function postInstallLinWin() {
   exec(baseCommand);
